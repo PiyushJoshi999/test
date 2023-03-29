61afb416433dfdcbc9442d7e39fdfe4acd9fc3e6
piyush_joshi <piyushjoshi470@gmail.com>
2023-03-29 17:55:19 +0530

    Adding demo.js

---
+++ Desktop/iyush/demo.js
@@ -0,0 +1,22 @@
+// Types of variables: let, const:
+//Types of data types: string, numbers, boolean, null, undefined, symbol.
+
+const name = "Piyush";
+const phone = 0123;
+const age = 24;
+const isCool = true;
+const rating = 4.9;
+
+//Concatination:
+console.log(My name is ${name} and my age is ${age});
+
+//Arrays : that hold multiple values:
+
+//Ways of declaring arrays:
+let rate = new Array(1,2,3,4,5);
+const fruits = ['apples', 'oranges', 'pears'];
+fruits[3] = 'mangoes';
+fruits.push('grapes');//if we want to add element in the array we can use push, if we don't know the size of the array.
+fruits.unshift('pineapple');//if we want to add the element at the beggning of the array, we use unshift method.
+fruits.pop();//we can use pop method to remove the last element in the array.
+
