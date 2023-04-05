129055a8a35985681fc1e347e4018e2b0708b363
piyush_joshi <piyushjoshi470@gmail.com>
2023-04-05 10:40:23 +0530

    Modifying task.js

--- Desktop/iyush/gitinonevedio/task.js
+++ Desktop/iyush/gitinonevedio/task.js
@@ -57,25 +57,117 @@
 // const lastItem = document.querySelector('.list-group-item:last-child');
 // lastItem.style.color = 'blue';
 
-const secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.color = 'green';
-secondItem.style.fontWeight = 'bold';
+// const secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.color = 'green';
+// secondItem.style.fontWeight = 'bold';
 
 
-//QUERY SELECTOR ALL//
+// //QUERY SELECTOR ALL//
 
-const titles = document.querySelectorAll('.title');
-console.log(titles);
-titles[0].textContent = 'Hello';
+// const titles = document.querySelectorAll('.title');
+// console.log(titles);
+// titles[0].textContent = 'Hello';
 
-const odd = document.querySelectorAll('li:nth-child(odd)');
+// const odd = document.querySelectorAll('li:nth-child(odd)');
 
-for(let i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor = 'green';
-}
+// for(let i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor = 'green';
+// }
 
 // const secItem = document.querySelectorAll('li:nth-child(2)');
 // secItem.style.color = 'green';
+
+//TRAVERSING THE DOM//
+
+const itemList = document.querySelector('#items');
+
+//parentNode property:
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentNode.parentNode);
+
+// //PARENT ELEMENT PROPERTY//
+
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentElement.parentElement);
+
+// //CHILD NODES//
+// console.log(itemList.childNodes);
+
+//CHILDREN NODE//
+
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = 'yellow';
+
+// //FIRST ELEMENT CHILD//
+
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Hello 1';
+
+//LAST ELEMENT CHILD//
+
+console.log(itemList.lastElementChild);
+itemList.lastElementChild.textContent = 'Hello 5';
+
+//nextElementSibling//
+
+console.log(itemList.nextElementSibling);
+
+//previousElementSibling//
+
+console.log(itemList.previousElementSibling);
+
+itemList.previousElementSibling.style.color = 'orange';
+
+//CREATE ELEMENT//
+
+//Create a div
+
+const newDiv = document.createElement('div');
+console.log(newDiv);
+
+//adding class to div
+
+newDiv.className = 'Hii';
+
+//adding id to div
+newDiv.id = 'hello';
+
+//adding attribute
+
+newDiv.setAttribute('title', 'Hello Div');
+
+//Create text node
+
+const newDivText = document.createTextNode('Hello Piyush');
+
+//adding text to div
+
+newDiv.appendChild(newDivText);
+
+const container = document.querySelector('header .container');
+
+const h1 = document.querySelector('header h1');
+console.log(newDiv);
+
+container.insertBefore(newDiv, h1);
+
+newDiv.style.fontSize = '30px';
+
+//Adding hello world before text1
+const nd = document.createElement('div');
+
+nd.className = 'text';
+nd.id = 'text1';
+nd.setAttribute('title', 'hii');
+const ndtext = document.createTextNode('Hello World');
+nd.appendChild(ndtext);
+const ap = document.querySelector('div .container');
+const h = document.querySelector('div h2');
+ap.insertBefore(nd, h);
+
 
 
 
