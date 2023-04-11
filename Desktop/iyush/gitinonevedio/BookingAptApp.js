c1abee3c5cdc3559b17c4bae3801e01a81c1a4be
piyush_joshi <piyushjoshi470@gmail.com>
2023-04-11 11:28:38 +0530

    Modifying js file to store the user data as a string of object

--- Desktop/iyush/gitinonevedio/BookingAptApp.js
+++ Desktop/iyush/gitinonevedio/BookingAptApp.js
@@ -1,4 +1,4 @@
-
+//In local storage we can only store string as an input....to store the the number in local storage we have to convert it to a string.
   function validateForm() {
     const firstName = document.getElementById('first-name').value;
     const lastName = document.getElementById('last-name').value;
@@ -21,10 +21,52 @@
       alert("Please enter your phone number");
       return false;
     }
-    localStorage.setItem('first-name', firstName);
-    localStorage.setItem('last-name', lastName);
-    localStorage.setItem('email', eMail);
-    localStorage.setItem('phone', Phone);
+    // localStorage.setItem('first-name', firstName);
+    // localStorage.setItem('last-name', lastName);
+    // localStorage.setItem('email', eMail);
+    // localStorage.setItem('phone', Phone);
+    var user = {
+        firstName : firstName,
+        lastName : lastName,
+        eMail : eMail,
+        Phone : Phone   
+    }
+
+    //Using JSON.stringify to convert the object into the string
+    localStorage.setItem('user', JSON.stringify(user));
+    //Converting string back into the normal javascript object.
+    let user_desearilized = JSON.parse(localStorage.getItem('user'));
+
+    //Redirecting the user to another page.
+    //window.location.href = "success.html";
     return true;
   }
+
+  //JS SCRIPT to store user details as an object in the local storage.
+
+  /* // Add an event listener to the form submit button
+	document.getElementById("user-form").addEventListener("submit", function(event) {
+		// Prevent the default form submission behavior
+		event.preventDefault();
+
+		// Get the form input values
+		var firstName = document.getElementById("first-name").value;
+		var lastName = document.getElementById("last-name").value;
+		var email = document.getElementById("email").value;
+		var phone = document.getElementById("phone").value;
+
+		// Create an object with the user data
+		var user = {
+			firstName: firstName,
+			lastName: lastName,
+			email: email,
+			phone: phone
+		};
+
+		// Store the user object in the local storage as a JSON string
+		localStorage.setItem("user", JSON.stringify(user));
+
+		// Redirect the user to another page
+		window.location.href = "success.html";
+	}); */
 
