0d19dd1ef5d42ee216c4aee31f9738461edd4aa4
Piyush <piyushjoshi470@gmail.com>
2023-05-24 18:42:08 +0530

    Modifying App.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import ExpenseItem from "./Components/ExpenseItem";
 
 function App() {
   const expenses = [
-    {title: 'Car Insurance', amount: 399, date: new Date(2023, 4, 24)},
-    {title: 'Toilet paper', amount: 100, date: new Date(2023, 4, 23)},
-    {title: 'Toothpaste', amount: 150, date: new Date(2023, 4, 22)},
-    {title: 'Dryfruits', amount: 500, date: new Date(2023, 4, 20)}
+    {title: 'Car Insurance', amount: 399, location: 'Car', date: new Date(2023, 4, 24)},
+    {title: 'Toilet paper', amount: 100, location: 'Home', date: new Date(2023, 4, 23)},
+    {title: 'Toothpaste', amount: 150, location: 'Care', date: new Date(2023, 4, 22)},
+    {title: 'Dryfruits', amount: 500, location: 'Kitchen', date: new Date(2023, 4, 20)}
   ];
   return (
     // <div className="App">
@@ -32,10 +32,10 @@ function App() {
 
     <div>
       <h2>Lets get started</h2>
-      <ExpenseItem title = {expenses[0].title} amount = {expenses[0].amount} date = {expenses[0].date}></ExpenseItem>
-      <ExpenseItem title = {expenses[1].title} amount = {expenses[1].amount} date = {expenses[1].date}></ExpenseItem>
-      <ExpenseItem title = {expenses[2].title} amount = {expenses[2].amount} date = {expenses[2].date}></ExpenseItem>
-      <ExpenseItem title = {expenses[3].title} amount = {expenses[3].amount} date = {expenses[3].date}></ExpenseItem>
+      <ExpenseItem title = {expenses[0].title} location = {expenses[0].location} amount = {expenses[0].amount} date = {expenses[0].date}></ExpenseItem>
+      <ExpenseItem title = {expenses[1].title} location = {expenses[1].location} amount = {expenses[1].amount} date = {expenses[1].date}></ExpenseItem>
+      <ExpenseItem title = {expenses[2].title} location = {expenses[2].location} amount = {expenses[2].amount} date = {expenses[2].date}></ExpenseItem>
+      <ExpenseItem title = {expenses[3].title} location = {expenses[3].location} amount = {expenses[3].amount} date = {expenses[3].date}></ExpenseItem>
     </div>
 
   );
