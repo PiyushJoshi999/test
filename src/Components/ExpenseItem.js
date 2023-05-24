33ebe7e97b562af22c06b20b6bcc7773cfa9aac9
Piyush <piyushjoshi470@gmail.com>
2023-05-24 18:45:02 +0530

    Importing ExpenseDate and ExpenseDetails in ExpenseItem

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,19 +1,22 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
 
 function ExpenseItem(props) {
-    const expenseDate = new Date(2023, 4, 24);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 399;
-    const locationOfExpenditure = 'Lifestyle';
-    return (<div className='expense-item'><h2>Expense Item!</h2>
-    <p>Food Rs 10.</p>
-    <p>Petrol Rs 100.</p>
-    <p>Movies Rs 200.</p>
-    <div>{props.date.toISOString()}</div>
-    <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>{props.amount}</div>
-    </div>
+    // const expenseDate = new Date(2023, 4, 24);
+    // const expenseTitle = 'Car Insurance';
+    // const expenseAmount = 399;
+    // const locationOfExpenditure = 'Lifestyle';
+
+    
+
+    return (<div className='expense-item'>
+    <h2>Expense Item!</h2>
+    
+    <ExpenseDate date = {props.date}></ExpenseDate>
+    
+   <ExpenseDetails title = {props.title} amount = {props.amount} location = {props.location}></ExpenseDetails>
+   
     </div>
     );
 }
