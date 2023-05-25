9e67694cd54810f063be7eb3bb6675f14287d0ed
Piyush <piyushjoshi470@gmail.com>
2023-05-25 21:24:27 +0530

    Using fat arrow functions

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,8 +1,9 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
+import React from 'react';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
     // const expenseDate = new Date(2023, 4, 24);
     // const expenseTitle = 'Car Insurance';
     // const expenseAmount = 399;
@@ -16,9 +17,40 @@ function ExpenseItem(props) {
     <ExpenseDate date = {props.date}></ExpenseDate>
     
    <ExpenseDetails title = {props.title} amount = {props.amount} location = {props.location}></ExpenseDetails>
-   
+
     </div>
     );
+
+//return (
+
+    // <div className='expense-item'>
+    
+    // <ExpenseDate date ={props.date} />
+    
+    // <div>
+    
+    // <h1>
+    
+    // Expense 1
+    
+    // </h1>
+    
+    // <h1>
+    
+    // Expense 1
+    
+    // </h1>
+    
+    // </div>
+    
+    // <div>
+    // return React.createElement('div', {className: expense-item},
+    //        React.createElement('ExpenseDate', {date: props.date},
+    //        React.createElement('div', {}, 
+    //        React.createElement('h1', {}, "Expense 1",
+    //        React.createElement('h1', {}, "Expense 1"))) ));
+    
+    
 }
 
 export default ExpenseItem;
