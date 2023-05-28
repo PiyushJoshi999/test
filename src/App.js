aee4d607ba2b57866398a90e8c7c1cc9e9b552f5
Piyush <piyushjoshi470@gmail.com>
2023-05-28 15:39:34 +0530

    Adding delete component button

--- src/App.js
+++ src/App.js
@@ -1,44 +1,38 @@
-//import logo from './logo.svg';
 
-import ExpenseItem from "./Components/ExpenseItem";
+import React, { useState } from 'react';
+import ExpenseItem from './Components/ExpenseItem';
 
-//import './App.css';
-
-
-function App() {
+const App = () => {
   const expenses = [
-    {title: 'Car Insurance', amount: 399, location: 'Car', date: new Date(2023, 4, 24)},
-    {title: 'Toilet paper', amount: 100, location: 'Home', date: new Date(2023, 4, 23)},
-    {title: 'Toothpaste', amount: 150, location: 'Care', date: new Date(2023, 4, 22)},
-    {title: 'Dryfruits', amount: 500, location: 'Kitchen', date: new Date(2023, 4, 20)}
+    { title: 'Car Insurance', amount: 399, location: 'Car', date: new Date(2023, 4, 24) },
+    { title: 'Toilet paper', amount: 100, location: 'Home', date: new Date(2023, 4, 23) },
+    { title: 'Toothpaste', amount: 150, location: 'Care', date: new Date(2023, 4, 22) },
+    { title: 'Dryfruits', amount: 500, location: 'Kitchen', date: new Date(2023, 4, 20) }
   ];
-  return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
 
+  const [expenseItems, setExpenseItems] = useState(expenses);
+
+  const deleteExpenseItem = (index) => {
+    const updatedItems = [...expenseItems];
+    updatedItems.splice(index, 1);
+    setExpenseItems(updatedItems);
+  };
+
+  return (
     <div>
       <h2>Lets get started</h2>
-      <ExpenseItem title = {expenses[0].title} location = {expenses[0].location} amount = {expenses[0].amount} date = {expenses[0].date}></ExpenseItem>
-      <ExpenseItem title = {expenses[1].title} location = {expenses[1].location} amount = {expenses[1].amount} date = {expenses[1].date}></ExpenseItem>
-      <ExpenseItem title = {expenses[2].title} location = {expenses[2].location} amount = {expenses[2].amount} date = {expenses[2].date}></ExpenseItem>
-      <ExpenseItem title = {expenses[3].title} location = {expenses[3].location} amount = {expenses[3].amount} date = {expenses[3].date}></ExpenseItem>
+      {expenseItems.map((expense, index) => (
+        <ExpenseItem
+          key={index}
+          title={expense.title}
+          location={expense.location}
+          amount={expense.amount}
+          date={expense.date}
+          onDelete={() => deleteExpenseItem(index)}
+        />
+      ))}
     </div>
-
   );
-}
+};
 
 export default App;
