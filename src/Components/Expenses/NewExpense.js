5b980b164016758877bf4b4892909c9c0d49c9c9
Piyush <piyushjoshi470@gmail.com>
2023-05-30 17:47:09 +0530

    Adding NewExpense component

---
+++ src/Components/Expenses/NewExpense.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ExpenseForm from './ExpenseForm';
+
+const NewExpense = () => {
+    return (
+        <div>
+             <ExpenseForm></ExpenseForm>
+        </div>
+       
+    );
+
+}
+export default NewExpense;
